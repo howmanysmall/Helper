a1aec9b6c7062a2b9e53b52f02e4418d8a442c34
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-03-21 12:40:54 -0600

    Update FastSpawn.lua

--- FastSpawn.lua
+++ FastSpawn.lua
@@ -1,19 +1,24 @@
 -- Spawns a new thread without waiting one step
--- Fastest implementation I know of
--- @author Validark
 
-local function FunctionWrapper(callback, ...)
-    coroutine.yield()
-    callback(...)
-end
+local unpack = unpack
+local ArgumentsBindable = Instance.new("BindableEvent")
+local VoidBindable = Instance.new("BindableEvent")
 
-local Bindable = Instance.new("BindableEvent")
-Bindable.Event:Connect(function(callback) callback() end)
+ArgumentsBindable.Event:Connect(function(Function, Pointer) Function(Pointer()) end)
+VoidBindable.Event:Connect(function(Function) Function() end)
 
-local function FastSpawn(callback, ...)
-    local func = coroutine.wrap(FunctionWrapper)
-	func(callback, ...)
-    Bindable:Fire(func)
+local function FastSpawn(Function, ...)
+	--- Spawns a new thread to run a function on without waiting one step
+	-- @param function Func The function to run on a new thread
+	-- @{...} parameters to pass to Func
+	
+	if ... ~= nil then
+		local Arguments = {...}
+		local function Pointer() return unpack(Arguments) end
+		ArgumentsBindable:Fire(Function, Pointer)
+	else
+		VoidBindable:Fire(Function)
+	end
 end
 
 return FastSpawn
