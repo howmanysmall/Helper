bc64315736f269705ab5ad77100ba8713939079b
howmanysmall <lhamil29@bruinmail.slcc.edu>
2020-01-02 11:37:04 -0700

    Update FastSpawn.lua
    No more obscuring errors.

--- FastSpawn.lua
+++ FastSpawn.lua
@@ -2,18 +2,16 @@
 -- Fastest implementation I know of
 -- @author Validark
 
-local function FunctionWrapper(callback, ...)
-    coroutine.yield()
-    callback(...)
-end
+local BindableEvent = Instance.new("BindableEvent")
+BindableEvent.Event:Connect(function(Function, Arguments)
+	Function(Arguments())
+end)
 
-local Bindable = Instance.new("BindableEvent")
-Bindable.Event:Connect(function(callback) callback() end)
-
-local function FastSpawn(callback, ...)
-    local func = coroutine.wrap(FunctionWrapper)
-	func(callback, ...)
-    Bindable:Fire(func)
+local function FastSpawn(Function, ...)
+	local Arguments = table.pack(...)
+	BindableEvent:Fire(Function, function()
+		return table.unpack(Arguments, 1, Arguments.n)
+	end)
 end
 
 return FastSpawn
